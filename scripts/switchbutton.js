e5e56ce8ca350fabdc16d5605c3e23c544dd2de3
Catt Small <cattsmall@gmail.com>
2013-04-10 03:20:52 -0400

    EVERYTHING WORKSSSSS
    Signed-off-by: Catt Small <cattsmall@gmail.com>

--- scripts/switchbutton.js
+++ scripts/switchbutton.js
@@ -1,13 +1,35 @@
-$(document).ready(function() {
-	$(".switch-button").click( function() {
+(function($) {
+	$(".switch-button").click( function(e) {
 		$(this).toggleClass("active");
+		e.preventDefault();
 	});
 	
-	$('.switch-button-group.rating > .switch-button').click( function(){
+	$('.switch-button-group.rating > .switch-button').click( function(e){
 		if ($(this).is(":not(active)")){
 			$(this).addClass("active");
 		}	
 		$(this).prevAll(".switch-button").addClass("active");
 		$(this).nextAll(".switch-button").removeClass("active");
+		e.preventDefault();
 	});
-});
+	
+	$('.switch-button.star-all').click( function(e){
+		if ($(this).is(".active")){
+			$(".switch-button-target.star").addClass("active");
+		}	
+		else {
+			$(".switch-button-target.star").removeClass("active");
+		}	
+		e.preventDefault();
+	});
+	
+	$('.switch-button.check-all').click( function(e){
+		if ($(this).is(".active")){
+			$(".switch-button-target.checkbox").prop('checked', true);
+		}	
+		else {
+			$(".switch-button-target.checkbox").prop('checked', false);
+		}	
+		e.preventDefault();
+	});
+})(jQuery);
